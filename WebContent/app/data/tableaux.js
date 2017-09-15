dcff3a1df76b352617e531d03688308384ef3dcd
Steven Nesztler <steven.nesztler@ac-nancy-metz.fr>
2017-09-15 18:20:45 +0200

    Mise en place des objets

--- WebContent/app/data/tableaux.js
+++ WebContent/app/data/tableaux.js
@@ -7,19 +7,21 @@ define(["jquery"], function($){
 			      {id: "serrure", x:"440.32", y:"376.32"}
 			],
 	        direction : {
-	            bas : "coffre-bas"
+	                bas : "coffre-bas",
+	                gauche : "coffre-bas"
 	        }
 		},
 		"coffre-bas" : {
             width:1024, height : 768,
             elements : [
-                  {id: "boite", x:"440.32", y:"376.32"},
-                  {id: "collier", x:"440.32", y:"376.32"},
-                  {id: "athame", x:"440.32", y:"376.32"},
-                  {id: "Nori", x:"440.32", y:"376.32"}
+                  {id: "boite", x:"90", y:"130"},
+                  {id: "collier", x:"113", y:"413"},
+                  {id: "athame", x:"368", y:"94"},
+                  {id: "Nori", x:"526", y:"197"}
             ],
             direction : {
-                haut : "coffre"
+                haut : "coffre",
+                droite : "coffre"
             }
         }
 	};
